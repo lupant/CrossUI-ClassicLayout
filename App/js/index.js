f968bdf8849335f0a576abbe8f4bf64276027c70
Luigi Pantuosco <lupant@gmail.com>
2024-04-18 06:56:33 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,22 +1,106 @@
 xui.Class('App', 'xui.Module',{
     Instance:{
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Tools
+            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
-            append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout5")
-            .setItems([{"id":"before", "pos":"before", "size":100, "min":50, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}, {"id":"main", "min":10, "size":450}, {"id":"after", "pos":"after", "size":50, "min":20, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}])
+            
+            append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout5")
+                .setItems([
+                    {
+                        "id" : "before",
+                        "pos" : "before",
+                        "size" : 100,
+                        "min" : 50,
+                        "max" : 400,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    },
+                    {
+                        "id" : "main",
+                        "min" : 10,
+                        "size" : 450
+                    },
+                    {
+                        "id" : "after",
+                        "pos" : "after",
+                        "size" : 50,
+                        "min" : 20,
+                        "max" : 400,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    }
+                ])
             );
-
-            host.ctl_layout5.append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout13")
-            .setItems([{"id":"before", "pos":"before", "size":150, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":true}, {"id":"main", "min":10, "size":530}, {"id":"after", "pos":"after", "size":120, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":false}])
-            .setType("horizontal")
-            , "main");
-
+            
+            host.ctl_layout5.append(
+                xui.create("xui.UI.Layout")
+                .setHost(host,"ctl_layout13")
+                .setItems([
+                    {
+                        "id" : "before",
+                        "pos" : "before",
+                        "size" : 150,
+                        "min" : 10,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : true
+                    },
+                    {
+                        "id" : "main",
+                        "min" : 10,
+                        "size" : 530
+                    },
+                    {
+                        "id" : "after",
+                        "pos" : "after",
+                        "size" : 120,
+                        "min" : 10,
+                        "locked" : false,
+                        "folded" : false,
+                        "hidden" : false,
+                        "cmd" : false
+                    }
+                ])
+                .setType("horizontal"),
+                "main"
+            );
+            
+            host.ctl_layout13.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button5")
+                .setDock("center")
+                .setLeft("13.714285714285714em")
+                .setTop("22.857142857142858em")
+                .setWidth("10.59047619047619em")
+                .setHeight("5.104761904761904em")
+                .setCaption("Ciao!")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "other",
+                        "target" : "msg",
+                        "args" : [
+                            "{getCookies()}",
+                            "Ciao",
+                            200,
+                            5000
+                        ],
+                        "method" : "message",
+                        "event" : 1
+                    }
+                ]),
+                "main"
+            );
+            
             return children;
-            // ]]Code created by CrossUI RAD Tools
+            // ]]Code created by CrossUI RAD Studio
         }
     }
 });
